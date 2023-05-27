98425fb491a9e55f00c9907a41903ef68f9c7e7d
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-05-27 10:18:25 +0200

    feat: small update

--- lib/src/extras.dart
+++ lib/src/extras.dart
@@ -1,18 +1,100 @@
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-Future<String?> getRealdebridToken(List<String> private) async {
+/// ...
+Future<String?> getTokenForRealdebrid(
+  ({String username, String password}) private,
+) async {
+  String payload;
+  URLRequest startup;
+  final browser = HeadlessInAppWebView();
+  await browser.dispose();
+  await browser.run();
+  startup = URLRequest(url: WebUri('https://real-debrid.com/'));
+  await browser.webViewController.clearCache();
+  await browser.webViewController.loadUrl(urlRequest: startup);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#allpage-login-top > span').click();";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#loginform > fieldset:nth-child(1) > input[type=text]').value = '${private.username}';";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 2));
+  payload = "document.querySelector('#loginform > fieldset:nth-child(3) > input[type=password]').value = '${private.password}';";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 2));
+  payload = "document.querySelector('#submit').click();";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#username') === null";
+  final invalid = await browser.webViewController.evaluateJavascript(source: payload) as bool;
+  if (invalid) return null;
+  startup = URLRequest(url: WebUri('https://real-debrid.com/apitoken'));
+  await browser.webViewController.loadUrl(urlRequest: startup);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#wrapper_global > div > div > input[type=text]').value;";
+  final results = await browser.webViewController.evaluateJavascript(source: payload);
+  return results;
+}
+
+/// ...
+Future<bool> setPairForRealdebrid(
+  ({String username, String password}) private,
+  String pincode,
+) async {
+  if (pincode.length != 8) return false;
+  String payload;
+  URLRequest startup;
+  final browser = HeadlessInAppWebView();
+  await browser.dispose();
+  await browser.run();
+  startup = URLRequest(url: WebUri('https://real-debrid.com/'));
+  await browser.webViewController.clearCache();
+  await browser.webViewController.loadUrl(urlRequest: startup);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#allpage-login-top > span').click();";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#loginform > fieldset:nth-child(1) > input[type=text]').value = '${private.username}';";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 2));
+  payload = "document.querySelector('#loginform > fieldset:nth-child(3) > input[type=password]').value = '${private.password}';";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 2));
+  payload = "document.querySelector('#submit').click();";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#username') === null";
+  final invalid = await browser.webViewController.evaluateJavascript(source: payload) as bool;
+  if (invalid) return false;
+  startup = URLRequest(url: WebUri('https://real-debrid.com/device'));
+  await browser.webViewController.loadUrl(urlRequest: startup);
+  await Future.delayed(const Duration(seconds: 6));
+  payload = "document.querySelector('#usercode').value = '$pincode';";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 4));
+  payload = "document.querySelector('body > div > div > div:nth-child(2) > div > div.panel-body > form > fieldset > div.pull-right > input').click();";
+  await browser.webViewController.evaluateJavascript(source: payload);
+  await Future.delayed(const Duration(seconds: 6));
+  return true;
+}
+
+/// ...
+Future<bool> setPairForSpotify(
+  ({String username, String password}) private,
+  String pincode,
+) async {
   throw UnimplementedError();
 }
 
-Future<bool> setRealdebridPair(List<String> private, String pairing) async {
-  throw UnimplementedError();
-}
-
-Future<bool> setTraktPair(List<String> private, String pairing) async {
+/// ...
+Future<bool> setPairForTrakt(
+  ({String username, String password}) private,
+  String pincode,
+) async {
+  if (pincode.length != 8) return false;
   String address;
   String payload;
   URLRequest startup;
-  if (pairing.length != 8) return false;
   final browser = HeadlessInAppWebView();
   await browser.dispose();
   await browser.run();
@@ -20,10 +102,10 @@ Future<bool> setTraktPair(List<String> private, String pairing) async {
   await browser.webViewController.clearCache();
   await browser.webViewController.loadUrl(urlRequest: startup);
   await Future.delayed(const Duration(seconds: 6));
-  payload = "document.querySelector('#user_login').value = '${private[0]}';";
+  payload = "document.querySelector('#user_login').value = '${private.username}';";
   await browser.webViewController.evaluateJavascript(source: payload);
   await Future.delayed(const Duration(seconds: 2));
-  payload = "document.querySelector('#user_password').value = '${private[1]}';";
+  payload = "document.querySelector('#user_password').value = '${private.password}';";
   await browser.webViewController.evaluateJavascript(source: payload);
   await Future.delayed(const Duration(seconds: 2));
   payload = "document.querySelector('#new_user > div.form-actions > input').click();";
@@ -37,7 +119,7 @@ Future<bool> setTraktPair(List<String> private, String pairing) async {
   startup = URLRequest(url: WebUri('https://trakt.tv/activate'));
   await browser.webViewController.loadUrl(urlRequest: startup);
   await Future.delayed(const Duration(seconds: 6));
-  payload = "document.querySelector('#code').value = '$pairing';";
+  payload = "document.querySelector('#code').value = '$pincode';";
   await browser.webViewController.evaluateJavascript(source: payload);
   await Future.delayed(const Duration(seconds: 2));
   payload = "document.querySelector('#auth-form-wrapper > form > div.form-actions > input').click();";
@@ -56,4 +138,12 @@ Future<bool> setTraktPair(List<String> private, String pairing) async {
   await Future.delayed(const Duration(seconds: 12));
   await browser.dispose();
   return true;
+}
+
+/// ...
+Future<bool> setPairForYoutube(
+    ({String username, String password}) private,
+    String pincode,
+    ) async {
+  throw UnimplementedError();
 }
